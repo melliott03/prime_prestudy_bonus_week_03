25ef02e8ba120a12619456b58b2be2ea5043d8f6
melliott03 <melliott03@gmail.com>
2016-01-29 12:02:54 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function plus(a) {
+	return a + 6;
+}
+
+plus(5);
+
+function times(m) {
+	return m * 4;
+}
+
+minus(3);
+
+function divide(d) {
+	return d / 2;
+}
+divide(d);
+
+function alltogethernow(e){
+	return plus(times(divide(e)));
+}
+alltogethernow(4);
